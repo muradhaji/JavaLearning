cbe7e7f9ff2b1ba90107618229e7f2cf9f756b45
muradhaji <muradhajiyev42@icloud.com>
2023-09-29 11:29:04 +0400

    HelloWorldWithEclipse project: comments and docs added

---
+++ Examples/HelloWorldWithEclipse/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.javalearning","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"com.javalearning","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- Examples/HelloWorldWithEclipse/src/com/javalearning/HelloWorld.java
+++ Examples/HelloWorldWithEclipse/src/com/javalearning/HelloWorld.java
@@ -1,9 +1,27 @@
 package com.javalearning;
 
+/**
+ * Hello World Application
+ * 
+ * @author Murad Hajiyev
+ */
+
 public class HelloWorld {
 
+	/**
+	 * Main method
+	 * 
+	 * @param args input arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+//		Single line comment
+		
+		/*
+		 * Multi line
+		 * comment
+		 */
 	}
 
 }
