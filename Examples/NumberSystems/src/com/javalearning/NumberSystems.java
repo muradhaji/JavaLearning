aab387e7a645298a9acda052277ddbae2757aedf
muradhaji <muradhajiyev42@icloud.com>
2023-10-01 16:56:15 +0400

    NumberTypes project added

---
+++ Examples/NumberSystems/src/com/javalearning/NumberSystems.java
@@ -0,0 +1,24 @@
+package com.javalearning;
+
+/**
+ * 
+ * @author Murad Hajiyev
+ */
+
+public class NumberSystems {
+
+	public static void main(String[] args) {
+		int dec = 42;
+		System.out.println("Decimal: " + dec);
+		
+		int bin = 0b00101010;
+		System.out.println("Binary: " + bin);
+		
+		int oct = 052;
+		System.out.println("Octal: " + oct);
+		
+		int hex = 0x2A;
+		System.out.println("Hexadecimal: " + hex);
+	}
+
+}
